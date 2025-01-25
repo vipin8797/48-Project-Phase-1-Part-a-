d12e3dd218c0e60a239d0b066ef293306d48c366
vipin yadav <cy7795151@gmail.com>
2025-01-25 17:47:31 +0530

    listing.js created for Lising Schema and model  into models dir.

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require('express');
 const mongoose = require('mongoose'); //mongoose for MongoDB
 const path = require('path');  //Path for ejs templates
 const methodOverride = require('method-override'); //method override fot put,patch,delete req
-
+const Listing = require('./models/listing');
 
 //mongoose connection to DB
-const DB = "whatsapp";
+const DB = "wanderlust2";
 async function main(){
      await mongoose.connect(`mongodb://127.0.0.1:27017/${DB}`)
     }
@@ -26,6 +26,23 @@ app.use(methodOverride('_method')); //method overide
 //Roote route
 app.get('/',(req,res)=>{
     res.send("server is working");
+})
+
+app.get('/testListing', async(req,res)=>{
+    let listing1 = new Listing({
+        title:"Beach",
+        description:"This is like Helll",
+        price:2000,
+        location:"Maldeves",
+        country:"India",
+    });
+    try{
+    await listing1.save();
+    res.send("listing1 is saved in db");
+    }catch(err){
+        console.log(err);
+        res.send("some error");
+    }
 })
 
 
